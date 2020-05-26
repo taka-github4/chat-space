84d7106aa6cac6bfe908d97f3044c53a797e7546
taka-github4 <65590087+taka-github4@users.noreply.github.com>
2020-05-26 14:37:49 +0900

    restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "messages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< Updated upstream
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create]
+=======
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+>>>>>>> Stashed changes
 end
