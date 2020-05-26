4132adb10e402f210dc68f173741ab210b5826ba
taka-github4 <65590087+taka-github4@users.noreply.github.com>
2020-05-26 14:36:21 +0900

    continue

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "messages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
 end
