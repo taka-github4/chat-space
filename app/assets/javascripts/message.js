7068d0e8affdb5863ce6299ff7a2474a9f1f608a
taka-github4 <65590087+taka-github4@users.noreply.github.com>
2020-06-03 17:20:46 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,4 @@
 $(function(){ 
-  // var last_message_id = $('.message:last').data("message-id");
-  // console.log(last_message_id);
   function buildHTML(message){
    if ( message.image ) {
      var html =
