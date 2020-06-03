5e60920b102050bd1999e8cde542d227549b5920
taka-github4 <65590087+taka-github4@users.noreply.github.com>
2020-06-03 16:49:49 +0900

    fns

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,10 @@
 $(function(){ 
+  // var last_message_id = $('.message:last').data("message-id");
+  // console.log(last_message_id);
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="main__comment__box">
+      `<div class="main__comment__box"data-message-id=${message.id}>
          <div class="main__comment__box__top">
            <div class="main__comment__box__top__left">
              ${message.user_name}
@@ -21,7 +23,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="main__comment__box">
+      `<div class="main__comment__box"data-message-id=${message.id}>
          <div class="main__comment__box__top">
            <div class="main__comment__box__top__left">
              ${message.user_name}
@@ -62,4 +64,37 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
 });
 })
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  var last_message_id = $('.main__comment__box:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log(messages)
+    if (messages.length !== 0) {
+   //追加するHTMLの入れ物を作る
+   var insertHTML = '';
+   //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+   $.each(messages, function(i, message) {
+     insertHTML += buildHTML(message)
+   });
+   //メッセージが入ったHTMLに、入れ物ごと追加
+   $('.main__comment').append(insertHTML);
+   $('.main__comment').animate({ scrollTop: $('.main__comment')[0].scrollHeight});
+  }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
 });
